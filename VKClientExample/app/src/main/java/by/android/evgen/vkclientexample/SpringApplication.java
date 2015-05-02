6279dd06ec1a07867b6efccfb5a22fe51cd81c12
evgenmeshkin <evgenmeshkin@gmail.com>
2015-05-02 15:05:45 +0300

    add Splunk MINT SDK for Android

--- VKClientExample/app/src/main/java/by/android/evgen/vkclientexample/SpringApplication.java
+++ VKClientExample/app/src/main/java/by/android/evgen/vkclientexample/SpringApplication.java
@@ -1,6 +1,7 @@
 package by.android.evgen.vkclientexample;
 
 import com.activeandroid.ActiveAndroid;
+import com.splunk.mint.Mint;
 
 /**
  * Created by evgen on 28.03.2015.
@@ -11,6 +12,7 @@ public class SpringApplication extends com.activeandroid.app.Application {
     public void onCreate() {
         super.onCreate();
         ActiveAndroid.initialize(this);
+        Mint.initAndStartSession(this, "67db0308");
     }
 
 }
