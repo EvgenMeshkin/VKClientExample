40fe35dc0e4b76cae0f84386041c777cbc837185
evgenmeshkin <evgenmeshkin@gmail.com>
2015-06-02 22:49:08 +0300

    inprogress fix bug

--- VKClientExample/app/src/main/java/by/android/evgen/vkclientexample/SpringApplication.java
+++ VKClientExample/app/src/main/java/by/android/evgen/vkclientexample/SpringApplication.java
@@ -1,12 +1,14 @@
 package by.android.evgen.vkclientexample;
 
+import android.app.Application;
+
 import com.activeandroid.ActiveAndroid;
 import com.splunk.mint.Mint;
 
 /**
  * Created by evgen on 28.03.2015.
  */
-public class SpringApplication extends com.activeandroid.app.Application {
+public class SpringApplication extends Application {
 
     @Override
     public void onCreate() {
